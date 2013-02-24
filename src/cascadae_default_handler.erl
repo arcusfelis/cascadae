1ed07af4d8831e1f2bf390163db933fcf1085a7f
Uvarov Michael <freeakk@gmail.com>
2013-02-24 18:00:46 +0400

    Made cascadae_defaul_handler working with new cowboy.

--- src/cascadae_default_handler.erl
+++ src/cascadae_default_handler.erl
@@ -1,15 +1,15 @@
 -module(cascadae_default_handler).
 -behaviour(cowboy_http_handler).
--export([init/3, handle/2, terminate/2]).
+-export([init/3, handle/2, terminate/3]).
 
 init({_Any, http}, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
     Headers = [{<<"Location">>, "/index.html"}],
-    {ok, Req2} = cowboy_http_req:reply(301, Headers, <<>>, Req),
+    {ok, Req2} = cowboy_req:reply(301, Headers, <<>>, Req),
     {ok, Req2, State}.
 
-terminate(_Req, _State) ->
+terminate(_Reason, _Req, _State) ->
     ok.
 
